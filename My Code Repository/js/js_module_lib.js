2da2b1b97327bb9bb32cc2205a1aefa2d4660bd8
JT Creates <84053338+JT-Creates@users.noreply.github.com>
2021-08-13 14:44:47 -0400

    Upload of other files and clean up of initial upload.

--- My Code Repository/js/js_module_lib.js
+++ My Code Repository/js/js_module_lib.js
@@ -93,3 +93,62 @@ function dragElement(elmnt, elmnt2, x_loc, y_loc, dw_eval, move_eval, up_eval) {
   eval(up_eval);
  }
 }
+
+//============= Navigation ==========
+function topofpage(){
+  var C = document.getElementByClass("button1").children;
+  for(var x = 0; x < C.length; x++){
+    C[x].addEventListener("click", top, false);
+    C[x].style.display = "none";
+}
+var showing = 0;
+  function top(){
+    if (showing == 0){
+      C[0].style.display = "none";
+      C[1].style.display = "block";
+      showing =1;
+    }
+    else {
+      C[1].style.display = "none";
+      C[0].style.display = "block";
+      showing = 0;
+    }
+}
+
+function goBack() {
+  window.history.back()
+  
+}
+
+// ============ Entrance Image ============
+var showing = 0
+function start(){
+startInterval();
+  var C = document.getElementById("carrot").children;
+  for(var x = 0; x < C.length; x++){
+    C[x].className +="imgtest";
+    C[x].style.display = "none";
+  }
+
+  C[0].style.display = "block";
+//  document.getElementById("program_button").addEventListener()
+}
+  function test(i){
+  var C = document.getElementById("carrot").children;
+    showing += i;
+    if (showing < 0){
+      C[0].style.display = "none";
+      C[C.length-1].style.display = "block";
+      showing = C.length-1;
+    }
+    else if (showing >= C.length) {
+      C[C.length-1].style.display = "none";
+      C[0].style.display = "block";
+      showing = 0;
+    }
+    else {
+      C[showing-i].style.display = "none";
+      C[showing].style.display = "block";
+    }
+  }
+
